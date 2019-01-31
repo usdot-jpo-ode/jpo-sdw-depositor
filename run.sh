dfbb58d867d4cf769ae69f15c2f41c3f15dd5f40
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-01-31 15:37:18 -0500

    Move DOCKER_HOST_IP to env file

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t jpo-sdw-depositor . && docker run --rm  -e DOCKER_HOST_IP=$DOCKER_HOST_IP --env-file sample.env jpo-sdw-depositor:latest
+docker build -t jpo-sdw-depositor . && docker run --rm  --env-file sample.env jpo-sdw-depositor:latest
