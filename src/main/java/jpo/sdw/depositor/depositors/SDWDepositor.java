07bcc47e53c50d5b2f750090b16e650d40c9ea32
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-03-04 15:21:22 -0500

    Add application/json header to messages

--- src/main/java/jpo/sdw/depositor/depositors/SDWDepositor.java
+++ src/main/java/jpo/sdw/depositor/depositors/SDWDepositor.java
@@ -4,6 +4,9 @@ import java.net.URI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
@@ -19,12 +22,16 @@ public class SDWDepositor extends RestDepositor<String> {
    @Override
    public void deposit(String message) {
       try {
-         ResponseEntity<String> result = this.getRestTemplate().postForEntity(this.getDestination(), message,
-               String.class);
-         logger.debug("Response received. Status: {}, Body: {}", result.getStatusCode(), result.getBody());
+         HttpHeaders headers = new HttpHeaders();
+         headers.setContentType(MediaType.APPLICATION_JSON);
+
+         HttpEntity<String> httpEntity = new HttpEntity<String>(message ,headers);
+         
+         ResponseEntity<String> result = this.getRestTemplate().postForEntity(this.getDestination(), httpEntity, String.class);
+         
+         logger.info("Response received. Status: {}, Body: {}", result.getStatusCode(), result.getBody());
       } catch (ResourceAccessException e) {
          logger.error("Failed to send message to destination", e);
       }
    }
-
 }
