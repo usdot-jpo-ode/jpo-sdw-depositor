2f42e01de1f5874bab8b95d19b148e9297aa6431
Daniel Stephenson <dstephenson@trihydrol.com>
2022-01-27 10:12:17 -0700

    Refactored KafkaConsumerFactory.java to allow for confluent cloud integration.

--- src/main/java/jpo/sdw/depositor/controller/KafkaConsumerFactory.java
+++ src/main/java/jpo/sdw/depositor/controller/KafkaConsumerFactory.java
@@ -15,12 +15,40 @@ public class KafkaConsumerFactory {
    public static KafkaConsumer<String, String> createConsumer(DepositorProperties depositorProperties) {
       Properties props = new Properties();
       props.put("bootstrap.servers", depositorProperties.getKafkaBrokers());
-      props.put("group.id", depositorProperties.getGroupId());
-      props.put("enable.auto.commit", "true");
-      props.put("auto.commit.interval.ms", "1000");
+
       props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
       props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-      return new KafkaConsumer<String, String>(props);
+      String kafkaType = System.getenv("KAFKA_TYPE");
+      if (kafkaType != null && kafkaType.equals("CONFLUENT")) {
+         addConfluentProperties(props);
+      }
+
+      props.put("group.id", depositorProperties.getGroupId());
+      props.put("enable.auto.commit", "true");
+      props.put("auto.commit.interval.ms", "1000");
+
+      return new KafkaConsumer<>(props);
+   }
+
+   private static void addConfluentProperties(Properties props) {
+      props.put("ssl.endpoint.identification.algorithm", "https");
+      props.put("security.protocol", "SASL_SSL");
+      props.put("sasl.mechanism", "PLAIN");
+
+      String username = System.getenv("CONFLUENT_KEY");
+      String password = System.getenv("CONFLUENT_SECRET");
+
+      if (username != null && password != null) {
+         String auth = "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                 "username=\"" + username + "\" " +
+                 "password=\"" + password + "\";";
+         props.put("sasl.jaas.config", auth);
+      }
+      else {
+         // logger.error("Environment variables CONFLUENT_KEY and CONFLUENT_SECRET are not set. Set these in the .env file to use Confluent Cloud");
+         // TODO: log error
+      }
+
    }
 }
